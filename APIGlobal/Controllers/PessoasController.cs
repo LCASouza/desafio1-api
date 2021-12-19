cd6d377d655e3a51b008bb4ee5eea76991fc2ae5
LCASouza <dxfzlucassouza@outlook.com>
2021-12-19 17:41:41 -0300

    First Commit

---
+++ APIGlobal/Controllers/PessoasController.cs
@@ -0,0 +1,90 @@
+﻿using APIGlobal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIGlobal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoasController : Controller
+    {
+        private static List<Pessoa> pessoas = new List<Pessoa>();
+
+        private bool tratamentoDeDadosSimples(string nome, string cpf, string uf, string data)
+        {
+            if (nome.Length >= 1 && cpf.Length == 11 && uf.Length == 2 && data.Length == 8)
+            {
+                if (pessoas.FindAll(x => x.Cpf == cpf).Count == 0)
+                { 
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        [HttpGet("listar-pessoas")]
+        public List<Pessoa> listarPessoas()
+        {
+            return pessoas;
+        }
+
+        [HttpGet("consultar-codigo/{id}")]
+        public Pessoa consultarCodigoPessoa([FromRoute] int id)
+        {
+            return pessoas.Find(x => x.Codigo == id);
+        }
+
+        [HttpGet("consultar-uf/{uf}")]
+        public List<Pessoa> consultarUfPessoa([FromRoute] string uf)
+        {
+            return pessoas.FindAll(x => x.Uf == uf);
+        }
+
+        [HttpPost("inserir-pessoa/{nome}/{cpf}/{uf}/{data}")]
+        public Pessoa inserirPessoa([FromRoute] string nome, [FromRoute] string cpf, [FromRoute] string uf, [FromRoute] string data)
+        {
+            if (tratamentoDeDadosSimples(nome, cpf, uf, data))
+            {
+                int codigoNovo = 1;
+                if (pessoas.Count > 0)
+                {
+                    Pessoa ultimaPessoa = pessoas.Last();
+                    codigoNovo = ultimaPessoa.Codigo+1;
+                }
+
+                Pessoa pessoa = new Pessoa(codigoNovo, nome, cpf, uf, data);
+                pessoas.Add(pessoa);
+                return pessoa;
+            }
+            return null;
+        }
+
+        [HttpPost("atualizar-pessoa/{nome}/{cpf}/{uf}/{data}")]
+        public Pessoa atualizarPessoa([FromRoute] Pessoa request)
+        {
+            if (tratamentoDeDadosSimples(request.Nome, request.Cpf, request.Uf, request.DataDeNascimento))
+            {
+                Pessoa pessoaAtualizada = pessoas.Find(x => x.Codigo == request.Codigo);
+                if (pessoaAtualizada != null)
+                {
+                    pessoas[request.Codigo] = pessoaAtualizada;
+                    return pessoaAtualizada;
+                }
+            }
+            return null;
+        }
+
+        [HttpDelete("excluir-pessoa-id/{id}")]
+        public string excluirPessoa([FromRoute] int id)
+        {
+            if (pessoas.Exists(x => x.Codigo == id))
+            {
+                int posicaoNaLista = id - 1;
+                pessoas.RemoveAll(x => x.Codigo == id);
+                return "Pessoa Removida com Sucesso!";
+            }
+            return "Pessoa Nao Encontrada";
+        }
+
+    }
+}
---
+++ APIGlobal/Models/Pessoa.cs
@@ -0,0 +1,77 @@
+﻿namespace APIGlobal.Models
+{
+    public class Pessoa
+    {
+        private int codigo;
+        private string nome, cpf, uf, dataDeNascimento;
+
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Cpf
+        {
+            get
+            {
+                return cpf;
+            }
+            set
+            {
+                cpf = value;
+            }
+        }
+
+        public string Uf
+        {
+            get
+            {
+                return uf;
+            }
+            set
+            {
+                uf = value;
+            }
+        }
+
+        public string DataDeNascimento
+        {
+            get
+            {
+                return dataDeNascimento;
+            }
+            set
+            {
+                dataDeNascimento = value;
+            }
+        }
+
+        public Pessoa(int codigo, string nome, string cpf, string uf, string dataDeNascimento)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.cpf = cpf;
+            this.uf = uf;
+            this.dataDeNascimento = dataDeNascimento;
+        }
+    }
+}
